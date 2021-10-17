5dc00f79c40b2f01fb78b2bcb8021edc1d7c32da
Harshith C <fewbackseven@gmail.com>
2021-10-18 04:03:34 +0530

    Add files via upload

--- Employee.cs
+++ Employee.cs
@@ -3,17 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using EmployeeObjects;
 
-namespace EmployeeObjects
+namespace DataAccessLogic
 {
-    public class Employee
+    public class EmployeeData
     {
-        public string EmployeeID { get; set; }
-        public string EmployeeName { get; set; }
-        public string Designation { get; set; }
+        public string EpmpID { get; set; }        
 
-        public string Department { get; set; }
+        SqlConnection sqlCon = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ToString());
+        public int AddEmployee(EmployeeObjects.Employee employee)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
 
-        public int Salary { get; set; }
+            SqlCommand sqlCmd = new SqlCommand("spSaveRecords", sqlCon)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            sqlCmd.Parameters.AddWithValue("@EmployeeID",employee.EmployeeID);
+            sqlCmd.Parameters.AddWithValue("@EmployeeName", employee.EmployeeName);
+            sqlCmd.Parameters.AddWithValue("@Desigantion", employee.Designation);
+            sqlCmd.Parameters.AddWithValue("@Department", employee.Department);
+            sqlCmd.Parameters.AddWithValue("@Salary", employee.Salary);
+            int i = sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            return i;
+        }
+
+        public DataSet GetEmployee(EmployeeObjects.Employee employee)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+
+            SqlDataAdapter sqlDa = new SqlDataAdapter("GetEmployees",sqlCon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDa.SelectCommand.Parameters.AddWithValue("@EmployeeID", employee.EmployeeID);
+            DataSet dt = new DataSet();
+            sqlDa.Fill(dt,"Employee");
+            return dt;
+        }
+
     }
 }
