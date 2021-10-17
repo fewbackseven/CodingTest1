4580b7ed24c99efa8ea25bef893f7182217b3b19
Harshith C <fewbackseven@gmail.com>
2021-10-18 04:04:48 +0530

    Add files via upload

--- Employee.cs
+++ Employee.cs
@@ -4,48 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
-using System.Data.SqlClient;
-using System.Configuration;
-using EmployeeObjects;
+using DataAccessLogic;
 
-namespace DataAccessLogic
+
+namespace BusinessLogic
 {
-    public class EmployeeData
+    public class Employee
     {
-        public string EpmpID { get; set; }        
-
-        SqlConnection sqlCon = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ToString());
+        DataAccessLogic.EmployeeData employeeData = new DataAccessLogic.EmployeeData();
         public int AddEmployee(EmployeeObjects.Employee employee)
         {
-            if (sqlCon.State == ConnectionState.Closed)
-                sqlCon.Open();
-
-            SqlCommand sqlCmd = new SqlCommand("spSaveRecords", sqlCon)
-            {
-                CommandType = CommandType.StoredProcedure
-            };
-            sqlCmd.Parameters.AddWithValue("@EmployeeID",employee.EmployeeID);
-            sqlCmd.Parameters.AddWithValue("@EmployeeName", employee.EmployeeName);
-            sqlCmd.Parameters.AddWithValue("@Desigantion", employee.Designation);
-            sqlCmd.Parameters.AddWithValue("@Department", employee.Department);
-            sqlCmd.Parameters.AddWithValue("@Salary", employee.Salary);
-            int i = sqlCmd.ExecuteNonQuery();
-            sqlCon.Close();
+            int i = employeeData.AddEmployee(employee);
             return i;
         }
 
         public DataSet GetEmployee(EmployeeObjects.Employee employee)
         {
-            if (sqlCon.State == ConnectionState.Closed)
-                sqlCon.Open();
-
-            SqlDataAdapter sqlDa = new SqlDataAdapter("GetEmployees",sqlCon);
-            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
-            sqlDa.SelectCommand.Parameters.AddWithValue("@EmployeeID", employee.EmployeeID);
-            DataSet dt = new DataSet();
-            sqlDa.Fill(dt,"Employee");
-            return dt;
+            DataSet ds= employeeData.GetEmployee(employee);
+            return ds;
         }
-
     }
 }
