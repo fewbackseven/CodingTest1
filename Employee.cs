429662b5ea1c01f4e3608fa0c2840feb1f19ec4a
Harshith C <fewbackseven@gmail.com>
2021-10-18 04:00:32 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeObjects
+{
+    public class Employee
+    {
+        public string EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Designation { get; set; }
+
+        public string Department { get; set; }
+
+        public int Salary { get; set; }
+    }
+}
